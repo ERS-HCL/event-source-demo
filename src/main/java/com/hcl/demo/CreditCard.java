0d76363ccd2a6b4fb94e9aafdc88fc1b2b0db730
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-05-09 09:33:16 +0530

    Added implementation to make test pass

--- src/main/java/com/hcl/demo/CreditCard.java
+++ src/main/java/com/hcl/demo/CreditCard.java
@@ -12,25 +12,40 @@ import java.util.UUID;
 @Getter
 public class CreditCard {
     private UUID uuid;
+    private BigDecimal limit;
+    private BigDecimal used=BigDecimal.ZERO;
 
     public CreditCard(UUID uuid){
         this.uuid = uuid;
     }
 
     void assignLimit(BigDecimal amount) {
+        if (limitAlreadyAssigned()){
+            throw new IllegalStateException();
+        }
+        this.limit = amount;
+    }
 
+    private boolean limitAlreadyAssigned() {
+        if (limit != null) return true;
+        else return false;
     }
 
     void withdraw(BigDecimal amount) {
+        if (notEnoughMoneyToWithdraw(amount)) throw new IllegalStateException();
+        this.used = used.add(amount);
+    }
 
+    private boolean notEnoughMoneyToWithdraw(BigDecimal amount) {
+        return availableLimit().compareTo((amount))<0;
     }
 
     void repay(BigDecimal amount) {
-
+        used = used.subtract(amount);
     }
 
     public BigDecimal availableLimit() {
-        return  BigDecimal.ZERO;
+        return limit.subtract(used);
     }
 
 
