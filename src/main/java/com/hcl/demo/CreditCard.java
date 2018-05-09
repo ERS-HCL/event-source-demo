5377a01137fb673c1630ab7617a1a208f1e7e38e
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-05-09 11:10:39 +0530

    Added implementation of Events and Added TDD Test for Repository expected functionality

--- src/main/java/com/hcl/demo/CreditCard.java
+++ src/main/java/com/hcl/demo/CreditCard.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.math.BigDecimal;
-import java.util.UUID;
+import java.util.*;
 
 @NoArgsConstructor
 @ToString
@@ -15,15 +15,26 @@ public class CreditCard {
     private BigDecimal limit;
     private BigDecimal used=BigDecimal.ZERO;
 
+    private final List<DomainEvent> dirtyEvents = new ArrayList<>();
+
+    public List<DomainEvent> getDirtyEvents() {
+        return Collections.unmodifiableList(dirtyEvents);
+    }
     public CreditCard(UUID uuid){
         this.uuid = uuid;
     }
 
-    void assignLimit(BigDecimal amount) {
-        if (limitAlreadyAssigned()){
-            throw new IllegalStateException();
+    void assignLimit(BigDecimal amount) { //command (allowed to change state)
+        if (limitAlreadyAssigned()){ //invariant
+            throw new IllegalStateException(); //NACK
         }
-        this.limit = amount;
+        //ACK (Name it in passed tense)
+        limitAssigned(new LimitAssigned(uuid,new Date(),amount));
+    }
+
+    private void limitAssigned(LimitAssigned limitAssigned) {
+        this.limit = limitAssigned.getLimit();
+        dirtyEvents.add(limitAssigned);
     }
 
     private boolean limitAlreadyAssigned() {
@@ -32,8 +43,15 @@ public class CreditCard {
     }
 
     void withdraw(BigDecimal amount) {
-        if (notEnoughMoneyToWithdraw(amount)) throw new IllegalStateException();
-        this.used = used.add(amount);
+        if (notEnoughMoneyToWithdraw(amount)) {
+            throw new IllegalStateException();
+        }
+        cardWithDrawn(new CardWithdrawn(uuid,amount,new Date()));
+    }
+
+    private void cardWithDrawn(CardWithdrawn cardWithdrawn) {
+        this.used = used.add(cardWithdrawn.getAmount());
+        dirtyEvents.add(cardWithdrawn);
     }
 
     private boolean notEnoughMoneyToWithdraw(BigDecimal amount) {
@@ -41,7 +59,12 @@ public class CreditCard {
     }
 
     void repay(BigDecimal amount) {
-        used = used.subtract(amount);
+        cardRepaid(new CardRepaid(uuid,amount,new Date()));
+    }
+
+    private void cardRepaid(CardRepaid cardRepaid) {
+        used = used.subtract(cardRepaid.getAmount());
+        dirtyEvents.add(cardRepaid);
     }
 
     public BigDecimal availableLimit() {
